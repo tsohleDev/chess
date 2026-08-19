9d326a19becbf0877ff37a41676e61c6952689f1
Tsohle <mokhemisitsohle@gmail.com>
2026-08-19 08:16:52 +0200

    init

---
+++ ChessEngine/Execptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessEngine
+{
+    internal class ChessExecption(String message) : Exception(message) { }
+
+
+    public record Errors
+    {
+        public static readonly Dictionary<int, string> Messages = new () {{ 1, "Invalid Move" }};
+    };
+}
