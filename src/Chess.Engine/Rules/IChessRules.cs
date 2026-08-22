45cd402decf2e64b40e605c13d335655ed64a835
Tsohle <mokhemisitsohle@gmail.com>
2026-08-22 12:54:09 +0200

    Add rules

---
+++ src/Chess.Engine/Rules/IChessRules.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessEngine.Rules
+{
+    internal interface IChessRules
+    {
+        public bool IsMoveValid(Piece from, Piece to);
+        public bool IsCheckMate();
+        public bool IsDraw();
+    }
+}
