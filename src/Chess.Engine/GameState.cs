b6260cc134d44219e5981cd8afd7541af41d9ed3
Tsohle <mokhemisitsohle@gmail.com>
2026-08-22 12:54:36 +0200

    Update state - include game history

---
+++ src/Chess.Engine/GameState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace ChessEngine;
+
+public sealed class GameState
+{
+    private static readonly Lazy<GameState> _instance
+        = new Lazy<GameState>(() => new GameState());
+
+    private GameState()
+    {
+        Squares = new BitArray(64);
+        PiecesFactory pieceFactory = new PiecesFactory();
+        ActivePieces = pieceFactory.Pieces;
+        CapturedPieces = [];
+        AlgebraicNotation = [];
+        Squares.SetAll(false);
+    }
+
+    public static GameState Instance
+    {
+        get { return _instance.Value; }
+    }
+
+    public BitArray Squares { get; private set; }
+    public List<Piece> ActivePieces { get; private set; }
+
+    public List<Piece> CapturedPieces { get; private set; }
+    public List<(Square, Square)> AlgebraicNotation { get; private set; }
+
+    public bool CheckMate { get; set; }
+
+    public bool Check { get; set; }
+
+    public Player Turn { get; set; }
+}
