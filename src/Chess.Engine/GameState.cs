945ee527bab292f59813c3023d90c1baaef84ea4
Tsohle <mokhemisitsohle@gmail.com>
2026-09-01 15:22:51 +0200

    Update gamestate record
    - make it zero allocation

--- src/Chess.Engine/GameState.cs
+++ src/Chess.Engine/GameState.cs
@@ -1,36 +1,3 @@
-﻿using System.Collections;
+﻿namespace ChessEngine;
 
-namespace ChessEngine;
-
-public sealed class GameState
-{
-    private static readonly Lazy<GameState> _instance
-        = new Lazy<GameState>(() => new GameState());
-
-    private GameState()
-    {
-        Squares = new BitArray(64);
-        PiecesFactory pieceFactory = new PiecesFactory();
-        ActivePieces = pieceFactory.Pieces;
-        CapturedPieces = [];
-        AlgebraicNotation = [];
-        Squares.SetAll(false);
-    }
-
-    public static GameState Instance
-    {
-        get { return _instance.Value; }
-    }
-
-    public BitArray Squares { get; private set; }
-    public List<Piece> ActivePieces { get; private set; }
-
-    public List<Piece> CapturedPieces { get; private set; }
-    public List<(Square, Square)> AlgebraicNotation { get; private set; }
-
-    public bool CheckMate { get; set; }
-
-    public bool Check { get; set; }
-
-    public Player Turn { get; set; }
-}
+public record struct GameState(ulong Squares, Piece[] Pieces, Player Turn);
