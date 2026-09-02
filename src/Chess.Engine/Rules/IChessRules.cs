8f55c0adcc0e5e40d61b5c76dac504fc8205a03a
Tsohle <mokhemisitsohle@gmail.com>
2026-09-03 00:14:49 +0200

    Remove Rules
    - Refactor to add rules in game.cs

--- src/Chess.Engine/Rules/IChessRules.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChessEngine.Rules;
-
-internal interface IChessRules
-{
-    public bool IsMoveValid(Piece from, Piece to);
-    public bool IsCheckMate(Square[] attackProfile);
-    public bool IsCheck(Square[] attackProfile);
-    public bool IsDraw();
-}
